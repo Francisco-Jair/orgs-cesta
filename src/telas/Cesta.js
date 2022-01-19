d9f536ef57ea56dd9eb2e793aa92b99110dea1cb
Francisco Jair <fjair123@ufpi.edu.br>
2022-01-19 01:08:58 -0300

    Primeiro commit

---
+++ src/telas/Cesta.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text } from "react-native";
+
+// Componente Funcional
+export default function Cesta() {
+  return <Text>Cesta</Text>;
+}
+
+// Componente de Classes
+// tinha que importar -> import React, { Component } from 'react';
+// class MeuComponente extends Component {
+//     render() {
+//       return (
+//         <View>
+//           <Text>Olá mundo!</Text>
+//         </View>
+//       );
+//     }
+//   }
+  
+//   export default MeuComponente;
